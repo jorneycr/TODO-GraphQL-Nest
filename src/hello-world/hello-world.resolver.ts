67c06c9ce8edeba16d2949d8836cf14f59186c82
jorneylopez <jorney95@live.com>
2023-03-29 23:34:31 -0600

    first query

--- src/hello-world/hello-world.resolver.ts
+++ src/hello-world/hello-world.resolver.ts
@@ -1,20 +1,22 @@
-import { Float, Query, Resolver, Int } from '@nestjs/graphql';
+import { Float, Query, Resolver, Int, Args } from '@nestjs/graphql';
 
 @Resolver()
 export class HelloWorldResolver {
 
-    @Query( ()=> String, {description: 'Hola mundo retorna', name: 'hello'} )
-    HelloWorld(): string{
+    @Query(() => String, { description: 'Hola mundo retorna', name: 'hello' })
+    HelloWorld(): string {
         return "Hola mundo";
     }
 
-    @Query( ()=> Float, {name: 'randomNUmber'})
+    @Query(() => Float, { name: 'randomNUmber' })
     getRandomNumber(): number {
         return Math.random() * 100;
     }
 
-    @Query(()=> Int, {name: 'randomFromZeroTo'})
-    getRandomFromZeroTo(): number {
-        return Math.floor(Math.random() * 10);
+    @Query(() => Int, { name: 'randomFromZeroTo', description: 'From zero to (default 6)' })
+    getRandomFromZeroTo(
+        @Args('to', { nullable: true, type: () => Int }) to: number = 6
+    ): number {
+        return Math.floor(Math.random() * to);
     }
 }
